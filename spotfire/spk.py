dcad84a8d4707e56d787b3f480f758c8b3c4046d
Joe Roberts <jorobert@tibco.com>
2021-03-25 10:12:51 -0700

    Update copyright year to year of initial release.

--- spotfire/spk.py
+++ spotfire/spk.py
@@ -1,4 +1,4 @@
-# Copyright © 2020. TIBCO Software Inc.
+# Copyright © 2021. TIBCO Software Inc.
 # This file is subject to the license terms contained
 # in the license file that is distributed with this file.
 
