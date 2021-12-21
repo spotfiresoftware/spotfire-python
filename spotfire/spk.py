3c3605d2fa420c0d9f13d27096c6c8b7cabad16b
Brian Bassett <bbassett@tibco.com>
2021-12-21 11:14:26 -0800

    Update Pylint checks from converting 'spotfire.codesign' module to Cython.

--- spotfire/spk.py
+++ spotfire/spk.py
@@ -639,7 +639,7 @@ class _CabPackageBuilder(_PackageBuilder):
 
     def _build_payload(self, metadata: ElementTree.Element, module: ElementTree.Element, payload_dest: str) -> None:
         """Build the main payload archive for the SPK package."""
-        # pylint: disable=import-outside-toplevel
+        # pylint: disable=import-outside-toplevel,no-name-in-module
         from spotfire import cabfile, codesign
         metadata_files = metadata.find("Files")
 
