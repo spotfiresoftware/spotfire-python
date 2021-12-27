784e10d0cd3e8cc5e5028ecea9bc8d7ec2bc020f
Brian Bassett <bbassett@tibco.com>
2021-12-27 14:28:15 -0800

    Split Windows API definitions out into separate files suitable for 'cimport'ing.

--- spotfire/codesign.pyx
+++ spotfire/codesign.pyx
@@ -11,112 +11,17 @@ IF UNAME_SYSNAME == "Windows":
 
     import os
 
-    # Windows typedefs
-    cdef extern from "windows.h":
-        ctypedef bint BOOL
-        ctypedef const Py_UNICODE* LPCWSTR
-        ctypedef unsigned long DWORD
-        ctypedef void* HANDLE
-        ctypedef int HRESULT
-        cdef enum:
-            S_OK
-        cdef DWORD GetLastError()
-        cdef HANDLE LoadLibrary(char*)
-        cdef void FreeLibrary(HANDLE)
-        cdef void* GetProcAddress(HANDLE, char*)
+    from vendor cimport windows
+    from vendor.windows cimport wincrypt, mssign32
 
-    # Definitions for crypt32.dll
-    cdef extern from "wincrypt.h":
-        cdef enum:
-            X509_ASN_ENCODING
-            PKCS_7_ASN_ENCODING
-        ctypedef struct CERT_CONTEXT:
-            pass
-        cdef enum:
-            CERT_KEY_SPEC_PROP_ID
-        ctypedef unsigned int ALG_ID
-        cdef enum:
-            CALG_SHA1
-            CALG_SHA_256
-        ctypedef struct CRYPT_ATTRIBUTES:
-            pass
-        ctypedef struct CRYPT_DATA_BLOB:
-            DWORD cbData
-            char* pbData
-        HANDLE PFXImportCertStore(CRYPT_DATA_BLOB*, LPCWSTR, DWORD)
-        BOOL CertCloseStore(HANDLE, DWORD)
-        cdef enum:
-            CERT_CLOSE_STORE_CHECK_FLAG
-        const CERT_CONTEXT* CertFindCertificateInStore(HANDLE, DWORD, DWORD, DWORD, void*, CERT_CONTEXT *)
-        cdef enum:
-            CERT_FIND_ANY
-        BOOL CertGetCertificateContextProperty(CERT_CONTEXT*, DWORD, void*, DWORD*)
-        BOOL CertFreeCertificateContext(CERT_CONTEXT*)
-
-    # Definitions for mssign32.dll
-    cdef extern from "mssign32.h":
-        ctypedef struct SIGNER_FILE_INFO:
-            DWORD cbSize
-            LPCWSTR pwszFileName
-            HANDLE hFile
-        ctypedef struct SIGNER_SUBJECT_INFO:
-            DWORD cbSize
-            DWORD* pdwIndex
-            DWORD dwSubjectChoice
-            SIGNER_FILE_INFO* pSignerFileInfo
-        cdef enum:
-            SIGNER_SUBJECT_FILE
-        ctypedef struct SIGNER_CERT_STORE_INFO:
-            DWORD cbSize
-            CERT_CONTEXT* pSigningCert
-            DWORD dwCertPolicy
-            HANDLE hCertStore
-        cdef enum:
-            SIGNER_CERT_POLICY_CHAIN
-        ctypedef struct SIGNER_CERT:
-            DWORD cbSize
-            DWORD dwCertChoice
-            SIGNER_CERT_STORE_INFO* pCertStoreInfo
-            HANDLE hwnd
-        cdef enum:
-            SIGNER_CERT_STORE
-        ctypedef struct SIGNER_ATTR_AUTHCODE:
-            DWORD cbSize
-            BOOL fCommercial
-            BOOL fIndividual
-            LPCWSTR pwszName
-            LPCWSTR pwszInfo
-        ctypedef struct SIGNER_SIGNATURE_INFO:
-            DWORD cbSize
-            ALG_ID algidHash
-            DWORD dwAttrChoice
-            SIGNER_ATTR_AUTHCODE* pAttrAuthcode
-            CRYPT_ATTRIBUTES* psAuthenticated
-            CRYPT_ATTRIBUTES* psUnauthenticated
-        cdef enum:
-            SIGNER_AUTHCODE_ATTR
-        ctypedef struct SIGNER_PROVIDER_INFO:
-            pass
-        ctypedef struct SIGNER_CONTEXT:
-            pass
-        ctypedef HRESULT (*SignerSignExType)(DWORD, SIGNER_SUBJECT_INFO*, SIGNER_CERT*, SIGNER_SIGNATURE_INFO*, SIGNER_PROVIDER_INFO*, LPCWSTR, CRYPT_ATTRIBUTES*, void*, SIGNER_CONTEXT**)
-        ctypedef HRESULT (*SignerTimeStampType)(SIGNER_SUBJECT_INFO*, LPCWSTR, CRYPT_ATTRIBUTES*, void*)
-        ctypedef HRESULT (*SignerTimeStampEx2Type)(DWORD, SIGNER_SUBJECT_INFO*, LPCWSTR, char*, CRYPT_ATTRIBUTES*, void*, SIGNER_CONTEXT**)
-        cdef enum:
-            SIGNER_TIMESTAMP_AUTHENTICODE
-            SIGNER_TIMESTAMP_RFC3161
-        char* OID_SHA1
-        char* OID_SHA256
-        ctypedef HRESULT (*SignerFreeSignerContextType)(SIGNER_CONTEXT*)
-
-    #
+    # An empty string.  Sometimes no passwords are encoded this way, sometimes as NULL.
     cdef Py_UNICODE _empty_wstring[1]
     _empty_wstring[:] = [0]
 
     cpdef void codesign_file(filename,
                              certificate,
-                             LPCWSTR password,
-                             LPCWSTR timestamp = NULL,
+                             windows.LPCWSTR password,
+                             windows.LPCWSTR timestamp = NULL,
                              bint use_rfc3161 = False,
                              bint use_sha256 = False):
         """Codesign a file using the Microsoft signing API found in mssign32.dll.
@@ -130,23 +35,23 @@ IF UNAME_SYSNAME == "Windows":
         :param use_sha256: whether or not to use SHA-256 as the timestamping hash function.  If ``True``, use SHA-256.
           If ``False``, use SHA-1.
         """
-        cdef HANDLE mssign32 = NULL
-        cdef SignerSignExType signer_sign_ex_fun
-        cdef SignerTimeStampType signer_time_stamp_fun
-        cdef SignerTimeStampEx2Type signer_time_stamp_ex2_fun
-        cdef SignerFreeSignerContextType signer_free_signer_context_fun
-        cdef CRYPT_DATA_BLOB cert_blob
-        cdef HANDLE cert_store = NULL
-        cdef const CERT_CONTEXT* cert_context = NULL
-        cdef DWORD key_spec, key_spec_len
-        cdef SIGNER_FILE_INFO signer_file_info
-        cdef SIGNER_SUBJECT_INFO signer_subject_info
-        cdef DWORD index
-        cdef SIGNER_CERT_STORE_INFO signer_cert_store_info
-        cdef SIGNER_CERT signer_cert
-        cdef SIGNER_ATTR_AUTHCODE signer_attr_authcode
-        cdef SIGNER_SIGNATURE_INFO signer_sig_info
-        cdef SIGNER_CONTEXT* signer_context = NULL
+        cdef windows.HANDLE mssign32_library = NULL
+        cdef mssign32.SignerSignExType signer_sign_ex_fun
+        cdef mssign32.SignerTimeStampType signer_time_stamp_fun
+        cdef mssign32.SignerTimeStampEx2Type signer_time_stamp_ex2_fun
+        cdef mssign32.SignerFreeSignerContextType signer_free_signer_context_fun
+        cdef wincrypt.CRYPT_DATA_BLOB cert_blob
+        cdef windows.HANDLE cert_store = NULL
+        cdef const wincrypt.CERT_CONTEXT* cert_context = NULL
+        cdef windows.DWORD key_spec, key_spec_len
+        cdef mssign32.SIGNER_FILE_INFO signer_file_info
+        cdef mssign32.SIGNER_SUBJECT_INFO signer_subject_info
+        cdef windows.DWORD index
+        cdef mssign32.SIGNER_CERT_STORE_INFO signer_cert_store_info
+        cdef mssign32.SIGNER_CERT signer_cert
+        cdef mssign32.SIGNER_ATTR_AUTHCODE signer_attr_authcode
+        cdef mssign32.SIGNER_SIGNATURE_INFO signer_sig_info
+        cdef mssign32.SIGNER_CONTEXT* signer_context = NULL
 
         try:
             # Sanity check arguments
@@ -158,82 +63,85 @@ IF UNAME_SYSNAME == "Windows":
                 raise Exception("SHA-256 timestamping requires the RFC 3161 timestamping protocol")
 
             # Load DLL and functions from mssign32.dll
-            mssign32 = LoadLibrary("mssign32.dll")
-            if mssign32 is NULL:
-                raise Exception(f"Cannot load mssign32.dll (0x{GetLastError():08x})")
-            signer_sign_ex_fun = <SignerSignExType>GetProcAddress(mssign32, "SignerSignEx")
+            mssign32_library = windows.LoadLibrary("mssign32.dll")
+            if mssign32_library is NULL:
+                raise Exception(f"Cannot load mssign32.dll (0x{windows.GetLastError():08x})")
+            signer_sign_ex_fun = <mssign32.SignerSignExType>windows.GetProcAddress(mssign32_library, "SignerSignEx")
             if signer_sign_ex_fun is NULL:
-                raise Exception(f"Cannot find function 'SignerSignEx' (0x{GetLastError():08x})")
-            signer_time_stamp_fun = <SignerTimeStampType>GetProcAddress(mssign32, "SignerTimeStamp")
+                raise Exception(f"Cannot find function 'SignerSignEx' (0x{windows.GetLastError():08x})")
+            signer_time_stamp_fun = <mssign32.SignerTimeStampType>windows.GetProcAddress(mssign32_library, "SignerTimeStamp")
             if signer_time_stamp_fun is NULL:
-                raise Exception(f"Cannot find function 'SignerTimeStamp' (0x{GetLastError():08x})")
-            signer_time_stamp_ex2_fun = <SignerTimeStampEx2Type>GetProcAddress(mssign32, "SignerTimeStampEx2")
+                raise Exception(f"Cannot find function 'SignerTimeStamp' (0x{windows.GetLastError():08x})")
+            signer_time_stamp_ex2_fun = <mssign32.SignerTimeStampEx2Type>windows.GetProcAddress(mssign32_library, "SignerTimeStampEx2")
             if signer_time_stamp_ex2_fun is NULL:
-                raise Exception(f"Cannot find function 'SignerTimeStampEx2' (0x{GetLastError():08x})")
-            signer_free_signer_context_fun = <SignerFreeSignerContextType>GetProcAddress(mssign32, "SignerFreeSignerContext")
+                raise Exception(f"Cannot find function 'SignerTimeStampEx2' (0x{windows.GetLastError():08x})")
+            signer_free_signer_context_fun = <mssign32.SignerFreeSignerContextType>windows.GetProcAddress(mssign32_library, "SignerFreeSignerContext")
             if signer_free_signer_context_fun is NULL:
-                raise Exception(f"Cannot find function 'SignerFreeSignerContext' (0x{GetLastError():08x})")
+                raise Exception(f"Cannot find function 'SignerFreeSignerContext' (0x{windows.GetLastError():08x})")
 
             # Open the certificate file and convert it into an in-memory cert store
             with open(certificate, "rb") as cert:
                 cert_data = cert.read()
-            cert_blob.cbData = <DWORD>len(cert_data)
+            cert_blob.cbData = <windows.DWORD>len(cert_data)
             cert_blob.pbData = <char*>cert_data
-            cert_store = PFXImportCertStore(&cert_blob, password, 0)
+            cert_store = wincrypt.PFXImportCertStore(&cert_blob, password, 0)
             if cert_store is NULL:
-                cert_store = PFXImportCertStore(&cert_blob, _empty_wstring, 0)
+                cert_store = wincrypt.PFXImportCertStore(&cert_blob, _empty_wstring, 0)
             if cert_store is NULL:
-                cert_store = PFXImportCertStore(&cert_blob, NULL, 0)
+                cert_store = wincrypt.PFXImportCertStore(&cert_blob, NULL, 0)
             if cert_store is NULL:
-                raise Exception(f"Could not load certificate; is the password correct? (0x{GetLastError():08x})")
+                raise Exception(f"Could not load certificate; is the password correct? (0x{windows.GetLastError():08x})")
 
             # Extract the cert from the new cert store
-            cert_context = CertFindCertificateInStore(cert_store, X509_ASN_ENCODING | PKCS_7_ASN_ENCODING, 0,
-                                                      CERT_FIND_ANY, NULL, NULL)
+            cert_context = wincrypt.CertFindCertificateInStore(cert_store,
+                                                               wincrypt.X509_ASN_ENCODING | wincrypt.PKCS_7_ASN_ENCODING,
+                                                               0, wincrypt.CERT_FIND_ANY, NULL, NULL)
             if cert_context is NULL:
-                raise Exception(f"Could not get certificate from store (0x{GetLastError():08x})")
+                raise Exception(f"Could not get certificate from store (0x{windows.GetLastError():08x})")
             found_private_key = False
             while not found_private_key:
                 key_spec_len = sizeof(key_spec)
-                has_private_key = CertGetCertificateContextProperty(cert_context, CERT_KEY_SPEC_PROP_ID,
-                                                                    &key_spec, &key_spec_len)
+                has_private_key = wincrypt.CertGetCertificateContextProperty(cert_context,
+                                                                             wincrypt.CERT_KEY_SPEC_PROP_ID,
+                                                                             &key_spec, &key_spec_len)
                 if has_private_key:
                     found_private_key = True
                 else:
-                    cert_context = CertFindCertificateInStore(cert_store, X509_ASN_ENCODING | PKCS_7_ASN_ENCODING, 0,
-                                                              CERT_FIND_ANY, NULL, cert_context)
+                    cert_context = wincrypt.CertFindCertificateInStore(cert_store,
+                                                                       wincrypt.X509_ASN_ENCODING | wincrypt.PKCS_7_ASN_ENCODING,
+                                                                       0, wincrypt.CERT_FIND_ANY, NULL, cert_context)
                     if cert_context is NULL:
-                        raise Exception(f"Could not get certificate from store (0x{GetLastError():08x})")
+                        raise Exception(f"Could not get certificate from store (0x{windows.GetLastError():08x})")
 
             # Prepare structures
-            signer_file_info.cbSize = sizeof(SIGNER_FILE_INFO)
+            signer_file_info.cbSize = sizeof(mssign32.SIGNER_FILE_INFO)
             signer_file_info.pwszFileName = filename
             signer_file_info.hFile = NULL
 
-            signer_subject_info.cbSize = sizeof(SIGNER_SUBJECT_INFO)
+            signer_subject_info.cbSize = sizeof(mssign32.SIGNER_SUBJECT_INFO)
             signer_subject_info.pdwIndex = &index
-            signer_subject_info.dwSubjectChoice = SIGNER_SUBJECT_FILE
+            signer_subject_info.dwSubjectChoice = mssign32.SIGNER_SUBJECT_FILE
             signer_subject_info.pSignerFileInfo = &signer_file_info
 
-            signer_cert_store_info.cbSize = sizeof(SIGNER_CERT_STORE_INFO)
+            signer_cert_store_info.cbSize = sizeof(mssign32.SIGNER_CERT_STORE_INFO)
             signer_cert_store_info.pSigningCert = cert_context
-            signer_cert_store_info.dwCertPolicy = SIGNER_CERT_POLICY_CHAIN
+            signer_cert_store_info.dwCertPolicy = mssign32.SIGNER_CERT_POLICY_CHAIN
             signer_cert_store_info.hCertStore = NULL
 
-            signer_cert.cbSize = sizeof(SIGNER_CERT)
-            signer_cert.dwCertChoice = SIGNER_CERT_STORE
+            signer_cert.cbSize = sizeof(mssign32.SIGNER_CERT)
+            signer_cert.dwCertChoice = mssign32.SIGNER_CERT_STORE
             signer_cert.pCertStoreInfo = &signer_cert_store_info
             signer_cert.hwnd = NULL
 
-            signer_attr_authcode.cbSize = sizeof(SIGNER_ATTR_AUTHCODE)
+            signer_attr_authcode.cbSize = sizeof(mssign32.SIGNER_ATTR_AUTHCODE)
             signer_attr_authcode.fCommercial = False
             signer_attr_authcode.fIndividual = True
             signer_attr_authcode.pwszName = _empty_wstring
             signer_attr_authcode.pwszInfo = NULL
 
-            signer_sig_info.cbSize = sizeof(SIGNER_SIGNATURE_INFO)
-            signer_sig_info.algidHash = CALG_SHA_256 if use_sha256 else CALG_SHA1
-            signer_sig_info.dwAttrChoice = SIGNER_AUTHCODE_ATTR
+            signer_sig_info.cbSize = sizeof(mssign32.SIGNER_SIGNATURE_INFO)
+            signer_sig_info.algidHash = wincrypt.CALG_SHA_256 if use_sha256 else wincrypt.CALG_SHA1
+            signer_sig_info.dwAttrChoice = mssign32.SIGNER_AUTHCODE_ATTR
             signer_sig_info.pAttrAuthcode = &signer_attr_authcode
             signer_sig_info.psAuthenticated = NULL
             signer_sig_info.psUnauthenticated = NULL
@@ -243,27 +151,29 @@ IF UNAME_SYSNAME == "Windows":
                                         &signer_context)
             if signer_context is not NULL:
                 signer_free_signer_context_fun(signer_context)
-            if result is not S_OK:
-                raise Exception(f"Could not sign file (0x{GetLastError():08x})")
+            if result is not windows.S_OK:
+                raise Exception(f"Could not sign file (0x{windows.GetLastError():08x})")
 
             # Timestamp the file
             if timestamp is not NULL:
                 if use_rfc3161:
-                    result = signer_time_stamp_ex2_fun(SIGNER_TIMESTAMP_RFC3161, &signer_subject_info, timestamp,
-                                                       OID_SHA256 if use_sha256 else OID_SHA1, NULL, NULL, &signer_context)
+                    result = signer_time_stamp_ex2_fun(mssign32.SIGNER_TIMESTAMP_RFC3161, &signer_subject_info,
+                                                       timestamp,
+                                                       mssign32.OID_SHA256 if use_sha256 else mssign32.OID_SHA1,
+                                                       NULL, NULL, &signer_context)
                     if signer_context is not NULL:
                         signer_free_signer_context_fun(signer_context)
                 else:
                     result = signer_time_stamp_fun(&signer_subject_info, timestamp, NULL, NULL)
-                if result is not S_OK:
-                    raise Exception(f"Could not timestamp file (0x{GetLastError():08x})")
+                if result is not windows.S_OK:
+                    raise Exception(f"Could not timestamp file (0x{windows.GetLastError():08x})")
         finally:
             if cert_context is not NULL:
-                CertFreeCertificateContext(cert_context)
+                wincrypt.CertFreeCertificateContext(cert_context)
             if cert_store is not NULL:
-                CertCloseStore(cert_store, CERT_CLOSE_STORE_CHECK_FLAG)
-            if mssign32 is not NULL:
-                FreeLibrary(mssign32)
+                wincrypt.CertCloseStore(cert_store, wincrypt.CERT_CLOSE_STORE_CHECK_FLAG)
+            if mssign32_library is not NULL:
+                windows.FreeLibrary(mssign32_library)
 
 ELSE:
 
