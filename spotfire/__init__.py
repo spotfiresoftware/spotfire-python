98bb8c59d2e78f6a2f91f1c76bcb94b8766ef75c
Brian Bassett <bbassett@tibco.com>
2023-02-23 15:01:45 -0800

    Update copyright notices. (#33)

--- spotfire/__init__.py
+++ spotfire/__init__.py
@@ -1,4 +1,4 @@
-# Copyright © 2021. TIBCO Software Inc.
+# Copyright © 2021. Cloud Software Group, Inc.
 # This file is subject to the license terms contained
 # in the license file that is distributed with this file.
 
