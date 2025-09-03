567838bc9125d8e7e58326b4f25369fd927eab23
vrane-tibco <158255115+vrane-tibco@users.noreply.github.com>
2025-09-03 14:20:41 +0530

    Update version to 2.5.0.dev0

--- spotfire/version.py
+++ spotfire/version.py
@@ -1,6 +1,6 @@
-# Copyright © 2021. Cloud Software Group, Inc.
+# Copyright © 2025. Cloud Software Group, Inc.
 # This file is subject to the license terms contained
 # in the license file that is distributed with this file.
 
 # pylint: skip-file
-__version__="2.4.0.dev0"
+__version__="2.5.0.dev0"
